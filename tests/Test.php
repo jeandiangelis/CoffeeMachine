ef4e74b597c0259a0af4c233c86ccbfa88dbaf51
Eugen Ganshorn <eugen.ganshorn@check24.de>
2019-03-19 17:14:12 +0100

    add and configure monolog

---
+++ tests/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eugenganshorn
+ * Date: 2019-03-19
+ * Time: 16:57
+ */
+
+namespace App\Tests;
+
+use TheFirstTest;
+
+class Test extends \PHPUnit_Framework_TestCase
+{
+}
